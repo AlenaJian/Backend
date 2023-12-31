2787413bbaeff05b1dbc08269f17a0462584b38a
chronic <alenaavni1999@gmail.com>
2023-12-31 11:59:48 +0530

    deploy

--- backend/connectDb/db.js
+++ backend/connectDb/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 const db = async()=>{
     
     try{
-       mongoose.connect(process.env.MONGODB_URI)
+       mongoose.connect('mongodb+srv://alena:kty5xO5cRu7LqYc8@cluster0.w7ykgib.mongodb.net/PK')
   .then(() => {
     console.log('MongoDB connected successfully');
     console.log(`mongoDb connected with server: ${mongoose.connection.host}`);
